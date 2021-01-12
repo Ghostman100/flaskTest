4ff9511289fa536743f38664f4979a26657608b8
Gennadiy <41668938+Ghostman100@users.noreply.github.com>
2021-01-12 18:32:12 +0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 CSRF_ENABLED = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 JSON_AS_ASCII = False
-SECRET_KEY = 'WpRMXn55OvFJ1aSjSnfvtobKNcBz7BPj'
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Foreveryoung33$@localhost:5432/poll_db"
+SECRET_KEY = 'SuperSecret'
+SQLALCHEMY_DATABASE_URI = "postgresql:///poll_db"
